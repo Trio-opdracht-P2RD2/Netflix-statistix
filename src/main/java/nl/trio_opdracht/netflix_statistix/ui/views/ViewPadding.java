e0fe67b83a4219e16368aed882c660fc4865dd57
Marc <marc.j.bouwman@gmail.com>
2018-01-13 20:49:12 +0100

    Add interfaces for the padding and font methods

---
+++ src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/ViewPadding.java
@@ -0,0 +1,5 @@
+package nl.trio_opdracht.netflix_statistix.ui.views;
+
+public interface ViewPadding {
+    void setPadding(int left, int top, int right, int bottom);
+}
---
+++ src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/ViewText.java
@@ -0,0 +1,11 @@
+package nl.trio_opdracht.netflix_statistix.ui.views;
+
+import nl.trio_opdracht.netflix_statistix.ui.FontType;
+
+public interface ViewText {
+    void setTextSize(int size);
+
+    void setFontType(FontType type);
+
+    void setFontName(String name);
+}
