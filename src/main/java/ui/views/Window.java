327d16dbf4877c2da75dc9db30696ba683067b4c
Marc <marc.j.bouwman@gmail.com>
2018-01-13 16:09:20 +0100

    Add Window - extending JFrame - to make it easier to use

---
+++ src/main/java/ui/views/Window.java
@@ -0,0 +1,19 @@
+package ui.views;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Window extends JFrame {
+    public Window(String title){
+        super(title);
+        setPreferredSize(new Dimension(1000, 800));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showWindow(){
+        pack();
+        setVisible(true);
+    }
+}
