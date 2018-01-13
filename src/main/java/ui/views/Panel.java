f5c129a847bb8d04fb33bf50373dc5d7aa8a2b5b
Marc <marc.j.bouwman@gmail.com>
2018-01-13 15:23:42 +0100

    Add a Panel, which extends JPanel, but has extra features

---
+++ src/main/java/ui/views/Panel.java
@@ -0,0 +1,26 @@
+package ui.views;
+
+import java.awt.LayoutManager;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Panel extends JPanel{
+    public Panel(){
+        super();
+    }
+
+    public Panel(LayoutManager layoutManager){
+        super(layoutManager);
+    }
+
+    public Panel(int boxLayoutDirection){
+        super();
+        setLayout(new BoxLayout(this, boxLayoutDirection));
+    }
+
+    public void setPadding(int left, int top, int right, int bottom){
+        setBorder(new EmptyBorder(top, right, bottom, left));
+    }
+}
