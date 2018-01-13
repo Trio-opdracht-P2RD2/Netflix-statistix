5f20167766146507195e38bd766660afc6e4ddf3
Marc <marc.j.bouwman@gmail.com>
2018-01-13 14:12:57 +0100

    Use PascalCase table titles and fix the '

--- docs/NetflixStatistix.sql
+++ docs/NetflixStatistix.sql
@@ -7,12 +7,12 @@ USE NetflixStatistix;
 
 
 -- Parent is PROGRAMMA
-DROP TABLE IF EXISTS PROGRAMMA;
-CREATE TABLE PROGRAMMA (
-    ProgrammaId int NOT NULL PRIMARY KEY
+DROP TABLE IF EXISTS Programma;
+CREATE TABLE Programma (
+    ID int NOT NULL PRIMARY KEY
 );
 
-INSERT INTO PROGRAMMA VALUES
+INSERT INTO Programma VALUES
     (1001),(1002),(1003),(1004),(1005),(1006),(1007),(1008),
     (1009),(1010),(2000),(2001),(2002),(2003),(2004),(2005),
     (2006),(2007),(2008),(2009),(2010),(2011),(2012),(2013),
@@ -25,28 +25,28 @@ INSERT INTO PROGRAMMA VALUES
 
 
 -- FK leidt af van de parent
-DROP TABLE IF EXISTS FILM;
-CREATE TABLE FILM (
-    Id int NOT NULL PRIMARY KEY,
+DROP TABLE IF EXISTS Film;
+CREATE TABLE Film (
+    ID int NOT NULL PRIMARY KEY,
     Titel varchar(50) NOT NULL,
     Leeftijdsindicatie varchar(50) NOT NULL,
     Taal varchar(50) NOT NULL,
     Tijdsduur varchar(8) NOT NULL,
     Genre varchar(50) NOT NULL, 
 
-    CONSTRAINT FILM_FK FOREIGN KEY (Id) REFERENCES PROGRAMMA(ProgrammaId)
+    CONSTRAINT FILM_FK FOREIGN KEY (ID) REFERENCES Programma(ID)
         ON UPDATE CASCADE 
         ON DELETE CASCADE
 );
 
-INSERT INTO FILM VALUES
+INSERT INTO Film VALUES
     (1010, 'The Abominable Bride', '12 jaar en ouder', 'Engels', '01:29', 'Detective'),
     (8001, 'The Life of Brian', '12 jaar en ouder', 'Engels', '01:34', 'Humor'),
     (8002, 'Pulp Fiction', '16 jaar en ouder', 'Engels-Amerikaans', '02:34', 'Misdaad' ),
     (8004, 'Pruimebloesem', '18 jaar en ouder', 'Nederlands', '01:20', 'Erotiek' ),
     (8008, 'Reservoir Dogs', '16 jaar en ouder', 'Engels-Amerikaans', '01:39', 'Misdaad'),
     (8010, 'The Good, the Bad and the Ugly', '12 jaar en ouder', 'Engels-Amerikaans', '02:41', 'Western' ),
-    (8011, 'Andy Warhol''s Dracula', '16 jaar en ouder', 'Engels-Amerikaans', '01:43', 'Humor'),
+    (8011, 'Andy Warhol’s Dracula', '16 jaar en ouder', 'Engels-Amerikaans', '01:43', 'Humor'),
     (8012, 'Ober', '6 jaar en ouder', 'Nederlands', '01:37', 'Humor'),
     (8014, 'Der Untergang', '6 jaar en ouder', 'Duits', '02:58', 'Oorlog'),
     (8016, 'De helaasheid der dingen', '12 jaar en ouder', 'Vlaams', '01:48', 'Humor'),
@@ -54,8 +54,8 @@ INSERT INTO FILM VALUES
 
 
 
-DROP TABLE IF EXISTS SERIE;
-CREATE TABLE SERIE (
+DROP TABLE IF EXISTS Serie;
+CREATE TABLE Serie (
     Serie varchar(50) NOT NULL PRIMARY KEY,
     Seizoen varchar(50) NOT NULL,
     Leeftijd varchar (50) NOT NULL,
@@ -64,7 +64,7 @@ CREATE TABLE SERIE (
     Lijkteenbeetjeop varchar(50) NOT NULL
 );
 
-INSERT INTO SERIE VALUES
+INSERT INTO Serie VALUES
     ('Sherlock', 'S01E01', '12 jaar en ouder', 'Engels', 'Detective', 'Fargo'),
     ('Breaking Bad', 'S01E01', '16 jaar en ouder', 'Engels-Amerikaans', 'Spanning', 'Fargo'),
     ('Fargo', 'S01E01', '16 jaar en ouder', 'Engels-Amerikaans', 'Spanning', 'Breaking Bad');
@@ -72,18 +72,18 @@ INSERT INTO SERIE VALUES
 
 
 -- FK leidt af van de parent
-DROP TABLE IF EXISTS AFLEVERING;
-CREATE TABLE AFLEVERING (
-    Id int NOT NULL PRIMARY KEY,
+DROP TABLE IF EXISTS Aflevering;
+CREATE TABLE Aflevering (
+    ID int NOT NULL PRIMARY KEY,
     Serie varchar(50) NOT NULL,
     Seizoen varchar(50) NOT NULL,
     Titel varchar(50) NOT NULL,
     Tijdsduur varchar(8) NOT NULL, 
 
-    CONSTRAINT AFLEVERING_FK FOREIGN KEY (Id) REFERENCES PROGRAMMA(ProgrammaId)
+    CONSTRAINT AFLEVERING_FK FOREIGN KEY (ID) REFERENCES Programma(ID)
         ON UPDATE CASCADE 
         ON DELETE CASCADE, 
-    CONSTRAINT AFLEVERING_FK2 FOREIGN KEY (Serie) REFERENCES SERIE(Serie)
+    CONSTRAINT AFLEVERING_FK2 FOREIGN KEY (Serie) REFERENCES Serie(Serie)
         ON UPDATE CASCADE 
         ON DELETE CASCADE
 );
@@ -99,11 +99,11 @@ INSERT INTO AFLEVERING VALUES
     (1008, 'Sherlock', 'S03E02', 'The Sign of Three', '01:28'), 
     (1009, 'Sherlock', 'S03E03', 'His Last Vow', '01:28'), 
     (2000, 'Breaking Bad', 'S01E01', 'Pilot', '00:58'), 
-    (2001, 'Breaking Bad', 'S01E02', 'Cat''s in the Bag…', '00:48'), 
+    (2001, 'Breaking Bad', 'S01E02', 'Cat’s in the Bag…', '00:48'), 
     (2002, 'Breaking Bad', 'S01E03', '…And the Bag’s in the River', '00:48'), 
     (2003, 'Breaking Bad', 'S01E04', 'Cancer Man', '00:48'), 
     (2004, 'Breaking Bad', 'S01E05', 'Gray Matter', '00:48'), 
-    (2005, 'Breaking Bad', 'S01E06', 'Crazy Handful of Nothin''', '00:48'), 
+    (2005, 'Breaking Bad', 'S01E06', 'Crazy Handful of Nothin’', '00:48'), 
     (2006, 'Breaking Bad', 'S01E07', 'A No-Rough-Stuff-Type Deal', '00:48'), 
     (2007, 'Breaking Bad', 'S02E01', 'Seven Thirty-Seven', '00:48'), 
     (2008, 'Breaking Bad', 'S02E02', 'Grilled', '00:48'), 
@@ -118,16 +118,16 @@ INSERT INTO AFLEVERING VALUES
     (2017, 'Breaking Bad', 'S02E11', 'Mandala', '00:48'), 
     (2018, 'Breaking Bad', 'S02E12', 'Phoenix', '00:48'), 
     (2019, 'Breaking Bad', 'S02E13', 'ABQ', '00:48'), 
-    (3001, 'Fargo', 'S01E01', 'The Crocodile''s Dilemma', '01:08'), 
+    (3001, 'Fargo', 'S01E01', 'The Crocodile’s Dilemma', '01:08'), 
     (3002, 'Fargo', 'S01E02', 'The Rooster Prince', '01:08'), 
     (3003, 'Fargo', 'S01E03', 'A Muddy Road', '01:08'), 
     (3004, 'Fargo', 'S01E04', 'Eating the Blame', '01:08'), 
     (3005, 'Fargo', 'S01E05', 'The Six Ungraspables', '01:08'), 
-    (3006, 'Fargo', 'S01E06', 'Buridan''s Ass', '01:08'), 
+    (3006, 'Fargo', 'S01E06', 'Buridan’s Ass', '01:08'), 
     (3007, 'Fargo', 'S01E07', 'Who Shaves the Barber?', '01:08'), 
     (3008, 'Fargo', 'S01E08', 'The Heap', '01:08'), 
     (3009, 'Fargo', 'S01E09', 'A Fox, a Rabbit, and a Cabbage', '01:08'), 
-    (3010, 'Fargo', 'S01E10', 'Morton''s Fork', '01:08)'), 
+    (3010, 'Fargo', 'S01E10', 'Morton’s Fork', '01:08)'), 
     (3101, 'Fargo', 'S02E01', 'Waiting for Dutch', '01:08'), 
     (3102, 'Fargo', 'S02E02', 'Before the Law', '01:08'), 
     (3103, 'Fargo', 'S02E03', 'The Myth of Sisyphus', '01:08'), 
@@ -142,8 +142,8 @@ INSERT INTO AFLEVERING VALUES
 
 
 -- Parent is ACCOUNT
-DROP TABLE IF EXISTS ACCOUNT;
-CREATE TABLE ACCOUNT (
+DROP TABLE IF EXISTS Account;
+CREATE TABLE Account (
     Abonneenummer int NOT NULL PRIMARY KEY,
     Naam varchar(50) NOT NULL,
     Straat varchar(50) NOT NULL,
@@ -152,7 +152,7 @@ CREATE TABLE ACCOUNT (
     Plaats varchar(50) NOT NULL
 );
 
-INSERT INTO ACCOUNT VALUES
+INSERT INTO Account VALUES
     (1215426, 'Fam. van Raalte', 'Schopenhauerdijkje', '3991ML', '5', 'Houten'),
     (5602533, 'J. van Betlehem', 'Nietzschestraat', '8542BE', '99', 'Breda'),
     (5285824, 'F. de Kat', 'Kantlaan', '8542CD', '11', 'Breda');
@@ -160,19 +160,19 @@ INSERT INTO ACCOUNT VALUES
 
 
 -- FK leidt af van de parent
-DROP TABLE IF EXISTS PROFIEL;
-CREATE TABLE PROFIEL (
+DROP TABLE IF EXISTS Profiel;
+CREATE TABLE Profiel (
     Abonneenummer int NOT NULL,
     Profielnaam varchar(50) NOT NULL,
     Geboortedatum date NOT NULL,
 
 	CONSTRAINT PK_PROFIEL PRIMARY KEY (Abonneenummer, Profielnaam),
-    CONSTRAINT PROFIEL_FK FOREIGN KEY (Abonneenummer) REFERENCES ACCOUNT(Abonneenummer)
+    CONSTRAINT PROFIEL_FK FOREIGN KEY (Abonneenummer) REFERENCES Account(Abonneenummer)
         ON UPDATE CASCADE 
         ON DELETE CASCADE
 );
 
-INSERT INTO PROFIEL VALUES
+INSERT INTO Profiel VALUES
     (1215426, 'Frank', '1968-01-25'),
     (1215426, 'Madelief', '2001-08-19'),
     (5602533, 'Petrus', '1999-06-26'),
@@ -183,23 +183,23 @@ INSERT INTO PROFIEL VALUES
 
 
 -- FK leidt af van de parent
-DROP TABLE IF EXISTS BEKEKEN;
-CREATE TABLE BEKEKEN (
+DROP TABLE IF EXISTS Bekeken;
+CREATE TABLE Bekeken (
     Abonneenummer int NOT NULL,
     Profielnaam varchar(50) NOT NULL,
     Gezien int NOT NULL,
     Percentage int NOT NULL CHECK (Percentage <= 100 AND Percentage >= 0),
 
     CONSTRAINT PK_BEKEKEN PRIMARY KEY (Abonneenummer, Profielnaam, Gezien),
-	CONSTRAINT BEKEKEN_FK FOREIGN KEY (Abonneenummer, Profielnaam) REFERENCES PROFIEL (Abonneenummer, Profielnaam)
+	CONSTRAINT BEKEKEN_FK FOREIGN KEY (Abonneenummer, Profielnaam) REFERENCES Profiel(Abonneenummer, Profielnaam)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT BEKEKEN_FK2 FOREIGN KEY (Gezien) REFERENCES PROGRAMMA (ProgrammaId)
+    CONSTRAINT BEKEKEN_FK2 FOREIGN KEY (Gezien) REFERENCES Programma(ID)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
-INSERT INTO BEKEKEN VALUES
+INSERT INTO Bekeken VALUES
     (1215426, 'Frank', 1001, 100),
     (1215426, 'Frank', 1002, 100),
     (1215426, 'Frank', 1003, 78),
