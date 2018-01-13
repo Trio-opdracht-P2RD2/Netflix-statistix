eb6ec95fdc27f83203cd699681ed96c82de9799b
Marc <marc.j.bouwman@gmail.com>
2018-01-13 11:48:52 +0100

    Add Button which extends JButton for an improved UI and easier usage of fonts

---
+++ src/main/java/ui/views/Button.java
@@ -0,0 +1,53 @@
+package ui.views;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
+
+import ui.FontType;
+
+public class Button extends JButton {
+    private String fontName;
+    private FontType fontType;
+    private int fontSize;
+
+    public Button(){
+        super();
+        init();
+    }
+
+    public Button(String text){
+        super(text);
+        init();
+    }
+
+    private void init(){
+        fontName = "Arial";
+        fontType = FontType.NORMAL;
+        fontSize = 16;
+        updateFont();
+        setBackground(Color.DARK_GRAY);
+        setBorder(new EmptyBorder(2, 2, 2, 2));
+    }
+
+    public void setTextSize(int size){
+        fontSize = size;
+        updateFont();
+    }
+
+    public void setFontType(FontType type){
+        fontType = type;
+        updateFont();
+    }
+
+    public void setFontName(String name){
+        fontName = name;
+        updateFont();
+    }
+
+    private void updateFont(){
+        setFont(new Font(fontName, fontType == FontType.BOLD ? Font.BOLD : fontType == FontType.ITALIC ? Font.ITALIC : Font.PLAIN, fontSize));
+    }
+}
