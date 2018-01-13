8157d23d334e0d8c7730be20258f1ea2cb376d2b
Marc <marc.j.bouwman@gmail.com>
2018-01-13 19:42:33 +0100

    Use try that auto-closes a stream/connection

--- src/main/java/nl/trio_opdracht/netflix_statistix/database/SQLConnection.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/database/SQLConnection.java
@@ -25,34 +25,17 @@ public class SQLConnection {
     }
 
     public void executeQuery(String database, String sql, QueryResultListener resultListener) {
-        // Connection beheert informatie over de connectie met de nl.trio_opdracht.netflix_statistix.database.
-        Connection con = null;
-
-        // Statement zorgt dat we een SQL query kunnen uitvoeren.
-        Statement stmt = null;
-
-        // ResultSet is de tabel die we van de nl.trio_opdracht.netflix_statistix.database terugkrijgen.
-        // We kunnen door de rows heen stappen en iedere kolom lezen.
-        ResultSet rs = null;
-
         try {
-            // 'Importeer' de driver die je gedownload hebt.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Maak de verbinding met de nl.trio_opdracht.netflix_statistix.database.
-            con = DriverManager.getConnection(createConnectionUrl(database));
-
-            // Stel een SQL query samen.
-            stmt = con.createStatement();
-            // Voer de query uit op de nl.trio_opdracht.netflix_statistix.database.
-            rs = stmt.executeQuery(sql);
-
-            if(resultListener != null) resultListener.onQueryResultReceived(rs);
+            try(Connection connection = DriverManager.getConnection(createConnectionUrl(database))){
+                try(Statement statement = connection.createStatement()){
+                    try(ResultSet resultSet = statement.executeQuery(sql)){
+                        if(resultListener != null) resultListener.onQueryResultReceived(resultSet);
+                    }
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (rs != null) try { rs.close(); } catch(Exception e) {}
-            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
-            if (con != null) try { con.close(); } catch(Exception e) {}
         }
     }
 }
