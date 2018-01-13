7ac5f288d393203278955f7c43d9f5ebc7f4631c
Marc <marc.j.bouwman@gmail.com>
2018-01-13 15:08:40 +0100

    Add a SideMenuView, which is a View which contains buttons

---
+++ src/main/java/ui/views/SideMenuView.java
@@ -0,0 +1,20 @@
+package ui.views;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+
+public class SideMenuView extends Panel {
+    public SideMenuView(){
+        super(BoxLayout.Y_AXIS);
+        setBackground(Color.WHITE);
+    }
+
+    public void addMenuItem(String title, ActionListener onClickListener){
+        Button button = new Button(title);
+        button.addActionListener(onClickListener);
+        button.setPadding(25, 10, 25, 10);
+        add(button);
+    }
+}
