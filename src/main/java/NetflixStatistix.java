61e312688880a97e5b25bc4f7da0bc5f564e3f61
Marc <marc.j.bouwman@gmail.com>
2018-01-13 10:44:35 +0100

    Rename Main to NetflixStatistix

--- src/main/java/NetflixStatistix.java
+++ src/main/java/NetflixStatistix.java
@@ -4,7 +4,7 @@ import database.SQLAccountName;
 import database.SQLConnection;
 import ui.UserInterface;
 
-public class Main {
+public class NetflixStatistix {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new UserInterface());
 
@@ -13,6 +13,6 @@ public class Main {
 
     private static void printBooks(){
         SQLConnection connection = new SQLConnection(SQLAccountName.MSSQLSERVER);
-        connection.executeQuery("SELECT TOP 10 * FROM Boek");
+        connection.executeQuery("Bibliotheek", "SELECT TOP 10 * FROM Boek");
     }
 }
