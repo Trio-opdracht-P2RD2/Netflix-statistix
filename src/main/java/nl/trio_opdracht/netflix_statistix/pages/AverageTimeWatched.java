35e2866af4ed6077c434ae8bf0556970b5586534
Marc <marc.j.bouwman@gmail.com>
2018-01-13 17:00:33 +0100

    Show the ID, Title and Genre of Movies as demo (will get removed later)

--- src/main/java/nl/trio_opdracht/netflix_statistix/pages/AverageTimeWatched.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/pages/AverageTimeWatched.java
@@ -1,16 +1,26 @@
 package nl.trio_opdracht.netflix_statistix.pages;
 
+import javax.swing.BoxLayout;
+
 import nl.trio_opdracht.netflix_statistix.Configuration;
 import nl.trio_opdracht.netflix_statistix.database.SQLConnection;
+import nl.trio_opdracht.netflix_statistix.ui.views.TextView;
 
 public class AverageTimeWatched extends Page {
+    private static final String query =
+            "SELECT *\n" +
+            "FROM Bekeken\n" +
+            "INNER JOIN Film ON Film.ID = Bekeken.Gezien;";
+
     public AverageTimeWatched(SQLConnection sqlConnection) {
         super(sqlConnection);
     }
 
     @Override protected void show() {
-        getSqlConnection().executeQuery(Configuration.databaseName, "", result -> {
-            // TODO: Add views
+        getContentView().setLayout(new BoxLayout(getContentView(), BoxLayout.Y_AXIS));
+        getSqlConnection().executeQuery(Configuration.databaseName, query, result -> {
+            while (result.next()) getContentView().add(new TextView(result.getInt("ID") + ": " + result.getString("Titel") + ", " + result.getString("Genre")));
+            getContentView().updateUI();
         });
     }
 
