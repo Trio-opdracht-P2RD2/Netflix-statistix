558df2049e4e49b2661156f0e03108591a640ac4
Marc <marc.j.bouwman@gmail.com>
2018-01-13 15:24:36 +0100

    Use Panel and setPadding

--- src/main/java/ui/views/BottomInfoView.java
+++ src/main/java/ui/views/BottomInfoView.java
@@ -3,15 +3,12 @@ package ui.views;
 import java.awt.Color;
 import java.awt.GridLayout;
 
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-public class BottomInfoView extends JPanel {
+public class BottomInfoView extends Panel {
     public BottomInfoView(String appName, String authors){
         super(new GridLayout(1, 2));
         setBackground(Color.WHITE);
         add(new TextView(appName));
         add(new TextView(authors));
-        setBorder(new EmptyBorder(2, 2, 2, 2));
+        setPadding(2, 2, 2, 2);
     }
 }
