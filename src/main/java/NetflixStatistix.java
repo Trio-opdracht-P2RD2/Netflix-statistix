a9186697a69565acc38e8e19d3e604d5fbf024a9
Marc <marc.j.bouwman@gmail.com>
2018-01-13 11:54:06 +0100

    Use the NetflixStatistix database and print Movies instead of Books

--- src/main/java/NetflixStatistix.java
+++ src/main/java/NetflixStatistix.java
@@ -9,28 +9,32 @@ public class NetflixStatistix {
         System.out.println("Welcome to Netflix Statistix!");
         SwingUtilities.invokeLater(new UserInterface());
 
-        printBooks();
+        printMovies();
     }
 
-    private static void printBooks(){
+    private static void printMovies(){
         System.out.println();
-        new SQLConnection(SQLAccountName.MSSQLSERVER).executeQuery("Bibliotheek", "SELECT TOP 10 * FROM Boek", result -> {
-            System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
-            System.out.println(String.format("| %7s | %-32s | %-24s |", "ISBN", "Titel", "Auteur"));
-            System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
+        new SQLConnection(SQLAccountName.MSSQLSERVER).executeQuery(
+                "NetflixStatistix",
+                "SELECT * " +
+                "FROM Film",
+                result -> {
+                    System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
+                    System.out.println(String.format("| %7s | %-32s | %-24s |", "ID", "Titel", "Genre"));
+                    System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
 
-            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
-            while (result.next()) {
-                // Vraag per row de kolommen in die row op.
-                int ISBN = result.getInt("ISBN");
-                String title = result.getString("Titel");
-                String author = result.getString("Auteur");
+                    // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
+                    while (result.next()) {
+                        // Vraag per row de kolommen in die row op.
+                        int ISBN = result.getInt("Id");
+                        String title = result.getString("Titel");
+                        String author = result.getString("Genre");
 
-                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
-                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
-                System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
-            }
-            System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
-        });
+                        // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
+                        // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
+                        System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
+                    }
+                    System.out.println(String.format("| %7s | %-32s | %-24s |", " ", " ", " ").replace(" ", "-"));
+                });
     }
 }
