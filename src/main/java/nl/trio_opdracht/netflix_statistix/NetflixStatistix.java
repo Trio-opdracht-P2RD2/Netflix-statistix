bf2c9f65751e70134c264cdcd3cb6045027f3295
Marc <marc.j.bouwman@gmail.com>
2018-01-14 21:08:22 +0100

    Add empty line
    This is actually not worth to have a commit

--- src/main/java/nl/trio_opdracht/netflix_statistix/NetflixStatistix.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/NetflixStatistix.java
@@ -13,6 +13,7 @@ import nl.trio_opdracht.netflix_statistix.ui.UserInterface;
 public class NetflixStatistix {
     public static void main(String[] args) {
         SQLConnection sqlConnection = new SQLConnection(Configuration.sqlAccountName);
+
         SwingUtilities.invokeLater(new UserInterface(
                 new AverageTimeWatched(sqlConnection),
                 new FilmsWatched(sqlConnection),
