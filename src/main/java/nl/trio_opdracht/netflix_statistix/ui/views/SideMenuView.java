2015f21c93b412a59d2b2d598ad3b5663430299e
Marc <marc.j.bouwman@gmail.com>
2018-01-14 17:25:10 +0100

    Add a background that is equal to the content pane when the button is selected

--- src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/SideMenuView.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/SideMenuView.java
@@ -1,6 +1,7 @@
 package nl.trio_opdracht.netflix_statistix.ui.views;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
@@ -8,6 +9,8 @@ import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
 
 import nl.trio_opdracht.netflix_statistix.ui.FontType;
+
+import static nl.trio_opdracht.netflix_statistix.Configuration.backgroundColor;
 
 public class SideMenuView extends ContainerView {
     public SideMenuView(){
@@ -18,7 +21,12 @@ public class SideMenuView extends ContainerView {
 
     public void addMenuItem(String title, ActionListener onClickListener){
         Button button = new Button(title);
-        button.addActionListener(onClickListener);
+        button.addActionListener(actionEvent -> {
+            for(Component component : getComponents()) component.setBackground(Color.LIGHT_GRAY);
+            button.setBackground(button.getModel().isRollover() ? backgroundColor.darker() : backgroundColor);
+
+            onClickListener.actionPerformed(actionEvent);
+        });
         button.setPadding(25, 20, 25, 20);
         button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getMinimumSize().height));
         button.setHorizontalAlignment(SwingConstants.LEFT);
