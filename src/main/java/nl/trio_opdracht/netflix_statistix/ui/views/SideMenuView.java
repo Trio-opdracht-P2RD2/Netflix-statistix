7a505ac9a82a62ca0651d510e33f5253c5c27bb2
Marc <marc.j.bouwman@gmail.com>
2018-01-14 15:44:25 +0100

    Let menu items use the full width

--- src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/SideMenuView.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/SideMenuView.java
@@ -1,9 +1,11 @@
 package nl.trio_opdracht.netflix_statistix.ui.views;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
 
 import nl.trio_opdracht.netflix_statistix.ui.FontType;
 
@@ -11,12 +13,15 @@ public class SideMenuView extends ContainerView {
     public SideMenuView(){
         super(BoxLayout.Y_AXIS);
         setBackground(Color.LIGHT_GRAY);
+        setPreferredSize(new Dimension(350, getPreferredSize().height));
     }
 
     public void addMenuItem(String title, ActionListener onClickListener){
         Button button = new Button(title);
         button.addActionListener(onClickListener);
         button.setPadding(25, 20, 25, 20);
+        button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getMinimumSize().height));
+        button.setHorizontalAlignment(SwingConstants.LEFT);
         button.setBackground(Color.LIGHT_GRAY);
         button.setFontType(FontType.BOLD);
         add(button);
