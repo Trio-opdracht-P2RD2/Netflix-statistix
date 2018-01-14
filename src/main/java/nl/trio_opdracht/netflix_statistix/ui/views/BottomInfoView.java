aef4fc877d03c4418973116736d5d3b0b28f2b30
Marc <marc.j.bouwman@gmail.com>
2018-01-14 17:26:02 +0100

    Add app icon to the bottom view

--- src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/BottomInfoView.java
+++ src/main/java/nl/trio_opdracht/netflix_statistix/ui/views/BottomInfoView.java
@@ -7,7 +7,9 @@ public class BottomInfoView extends ContainerView {
     public BottomInfoView(String appName, String authors){
         super(new GridLayout(1, 2));
         setBackground(Color.WHITE);
-        add(new TextView(appName));
+        TextView app = new TextView(appName);
+        app.setImageUrl("appicon.png");
+        add(app);
         add(new TextView(authors));
         setPadding(4, 4, 4, 4);
     }
